846a92eea26941e978d5f23fcf4b8fbeb61110a5
janick.norman@gmail.com <yanicknorman@gmail.com>
2018-07-04 09:51:38 +0700

    tambah

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -52,7 +52,7 @@ class LoginController extends Controller
       }
 
       if ($this->attemptLogin($request)) {
-          dd("hello" . \Auth::user()->name);
+          dd("Hello " . \Auth::user()->name . "!");
         /*==========================================================================================================
 
         	getticket.php
